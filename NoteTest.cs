5ce4a7b5254f37815855f94d4649e03d4ee178a4
neskol <hs-neskol@outlook.com>
2022-05-28 11:04:34 +0800

    Upload

---
+++ NoteTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static MaichartConverter.Note;
+using static MaichartConverter.Chart;
+using MaichartConverter;
+
+namespace MaichartConverter
+{
+	[TestClass]
+	public class NoteTest
+	{
+        [TestMethod]
+        public void GetBPMTimeUnitTest1()
+        {
+            double bpm = 60.0;
+            double expected = 4.0 / 384.0;
+            Assert.AreEqual(expected, Chart.GetBPMTimeUnit(bpm));
+        }
+    }
+}
+
