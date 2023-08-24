7e1061f5188e13238b6a223a35c1f0461b8c7c9b
Neskol Lu <hs-neskol@outlook.com>
2023-08-24 15:05:52 +0800

    Fixed each connecting slide

--- MaichartConverterUnitTest/Ma2ParserFestivalTest.cs
+++ MaichartConverterUnitTest/Ma2ParserFestivalTest.cs
@@ -10,13 +10,14 @@ namespace MaichartConverterUnitTest
         [TestMethod]
         public void FestivalTestTanaka()
         {
-            Ma2 test = new Ma2("/Users/Neskol/MaiAnalysis/A000/music/music011489/011489_03.ma2");
+            Ma2 test = new Ma2("../../../data/011489_03.ma2");
             Simai slideGroupCandidate = new Simai(test);
             // Console.WriteLine("BEFORE: ");
             // Console.WriteLine(slideGroupCandidate.Compose());
             // slideGroupCandidate.ComposeSlideGroup();
             // slideGroupCandidate.ComposeSlideEachGroup();
             slideGroupCandidate.Update();
+            Console.WriteLine(slideGroupCandidate.Compose());
             // foreach (Note x in slideGroupCandidate.Notes)
             // {
             //     Console.WriteLine("{0}, {1}", x.Compose(1),x.TimeStamp);
@@ -37,6 +38,25 @@ namespace MaichartConverterUnitTest
             slideGroupCandidate.Update();
             Console.WriteLine("AFTER: ");
             Console.WriteLine(slideGroupCandidate.Compose());
+            Assert.IsNotNull(test);
+        }
+
+        [TestMethod]
+        public void FestivalTest411()
+        {
+            Ma2 test = new Ma2("../../../data/011469_02.ma2");
+            Simai slideGroupCandidate = new Simai(test);
+            Console.WriteLine(test.Compose());
+            // Console.WriteLine("BEFORE: ");
+            // Console.WriteLine(slideGroupCandidate.Compose());
+            // slideGroupCandidate.ComposeSlideGroup();
+            // slideGroupCandidate.ComposeSlideEachGroup();
+            // slideGroupCandidate.Update();
+            // foreach (Note x in slideGroupCandidate.Notes)
+            // {
+            //     Console.WriteLine("{0}, {1}", x.Compose(1),x.TickTimeStamp);
+            // }
+            // Console.WriteLine(slideGroupCandidate.Compose());
             Assert.IsNotNull(test);
         }
 
