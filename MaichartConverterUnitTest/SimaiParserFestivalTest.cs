088c28a04d8ef02fb9d1a125ee67286b51a181d8
Neskol Lu <hs-neskol@outlook.com>
2023-11-22 21:33:10 +0800

    Added test for scanner; debug needed

--- MaichartConverterUnitTest/SimaiParserFestivalTest.cs
+++ MaichartConverterUnitTest/SimaiParserFestivalTest.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MaiLib;
+using System.IO;
 
 [TestClass]
 public class SimaiParserFestivalTest
@@ -22,5 +23,19 @@ public class SimaiParserFestivalTest
         Ma2 toMa2 = new Ma2(candidate);
         toMa2.ChartVersion = ChartEnum.ChartVersion.Ma2_104;
         Console.WriteLine(toMa2.Compose());
+    }
+
+    [TestMethod]
+    public void SimaiFestivalScannerInternetOverdose()
+    {
+        string candidate = File.ReadAllText(@"../../../data/maidata_internet_overdose.txt").Split("&inote_5")[1];
+        SimaiScanner scanner = new(candidate);
+        List<TokenEnum.TokenType> scannedTokens = new();
+        while (scanner.CurrentToken is not TokenEnum.TokenType.EOS)
+        {
+            scannedTokens.Add(scanner.CurrentToken);
+            scanner.ScanAndAdvance();
+            Console.WriteLine("At Line {0} Char {1}, token is {2}",scanner.LineNum, scanner.CharNum, scanner.CurrentToken);
+        }
     }
 }
