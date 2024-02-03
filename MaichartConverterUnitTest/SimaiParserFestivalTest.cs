fdfa7e89a2c262312c82aa22fd697b386677ae97
Neskol Lu <hs-neskol@outlook.com>
2024-02-03 14:44:48 +0800

    MaiLib update

--- MaichartConverterUnitTest/SimaiParserFestivalTest.cs
+++ MaichartConverterUnitTest/SimaiParserFestivalTest.cs
@@ -60,7 +60,7 @@ public class SimaiParserFestivalTest
         // string token = "(120){1}2-4[1:1]-6[1:1]-8[1:1],E";
         // string token = "(120){1}2-4-6-8[1:1],E";
         // string token = "(120){1}3-8^7[8:1],E";
-        string token = "(120){1}1V75V71[8:1]*V35V31[8:1],E";
+        string token = "(120){1}1V75V71[8:1]b*V35V31[8:1],E";
         Chart candidate = parser.ChartOfToken(tokenizer.TokensFromText(token));
         candidate = new Ma2(candidate);
         candidate.ChartVersion = ChartEnum.ChartVersion.Ma2_104;
