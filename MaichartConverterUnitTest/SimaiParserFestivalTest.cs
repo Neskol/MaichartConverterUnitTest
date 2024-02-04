9dff638fb3b777578e8977f033af90925d1bd4ba
Neskol Lu <hs-neskol@outlook.com>
2024-02-04 16:57:40 +0800

    MaiLib update

--- MaichartConverterUnitTest/SimaiParserFestivalTest.cs
+++ MaichartConverterUnitTest/SimaiParserFestivalTest.cs
@@ -43,9 +43,9 @@ public class SimaiParserFestivalTest
     [TestMethod]
     public void SimaiFestivalTestAssignConnectingSlideTiming()
     {
-        string token = "2-4-6-8[0.3##0.6]";
+        // string token = "2-4-6-8[0.3##0.6]";
         // string token = "2-4[1:1]-6[1:1]-8[1:1]";
-        // string token = "2-4[1:1]";
+        string token = "2!-4[1:1]";
         foreach (string x in SimaiParser.ExtractConnectingSlides(token))
         {
             Console.WriteLine(x);
